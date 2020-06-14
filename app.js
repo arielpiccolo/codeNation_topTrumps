4990879f8a31267ecd11843b7b87f7e718c92ed7
arielpiccolo <arielpiccolo@gmail.com>
2020-06-14 10:00:14 +0100

    update

--- app.js
+++ app.js
@@ -1,79 +1,91 @@
-//todo: to-do's
-//! comment
-//? comment
-
-//todo: set all the const variables with messages to the user
-const win = "you won";
-const lose = "You lose";
-const draw =
-  "its a draw, both cards will go to limbo, whoever wins the next cards takes also cards in limbo";
-const pickNext = "Play next card";
-
-//todo: set rule for the game
-/** pseudo code
- *  single only at the moment
- *  one card only is reveal at the time
- *  turns cannot be skip
- *  if ether player === 0 cards then > the other player win!
- *
- */
-
-//todo: set game functions(tuns,wins,loses,limbo ect)
-//single only at the moment, one card only is reveal at the time, turns cannot be skip
-//todo: set game data(statistics) explore arrays and classes choses
-
-//! trying with classes
-//todo: create a parent class to hold all the cards in it
-//todo: create sub-classes for each different card with only one attribute at the moment for easy testing
-
-
-//? =======================================================================================
-
-class Cards {
-    constructor(name, theme, strength) {
-        this._name = name;
-        this._theme = theme;
-        this._strength = strength;
-    }
-    
-    get name() {
-      return this._name;
-    }
-    get theme() {
-      return this._theme;
-    }
-    get strength() {
-      return this._strength; 
-    }
-        
-}
 
 
 
-class Monster extends Cards {
-    constructor(name, theme, strength) {
-      super(name, theme, strength);
-      
-     }
- 
-}
-
-
-const monster1 = new Monster('Dracula', 'Monster', 91);
-const monster2 = new Monster('Wolfman', 'Monster', 78);
-const monster3 = new Monster('The Beast', 'Monster', 87);
-
-
-
-function go(mons1, mons2) {
-  if (mons1 > mons2) {
-    return console.log(mons1);
- } else {
-   return console.log(mons2);
- }
+let monsters = [
   
-}
-
-go(monster2.strength, monster3.strength);
-
-
+    {
+      name : "Dracula",
+      power: 92
+     },
+    
+     {
+      name: "Wolfman",
+      power: '78 points' 
+     },
+    
+     {
+      name: "The Beast",
+      power: 87
+     }
+  
+  ];
+  
+  
+  
+  function monster(name, power) {
+    
+  }
+     
+  
+  
+   console.log(monsters[1].name + ' ' +  monsters[1].power) 
+    
+  
+  
+  
+  
+  
+  
+  /**
+  
+    monster1:"Dracula", theme:"Monster", strength:91,
+    monster2:"Wolfman", theme:"Monster", strength:78,
+    monster3:"The Beast", theme:"Monster", strength:87
+  
+  
+  class Cards {
+      constructor(name, theme, strength) {
+          this._name = name;
+          this._theme = theme;
+          this._strength = strength;
+      }
+      
+      get name() {
+        return this._name;
+      }
+      get theme() {
+        return this._theme;
+      }
+      get strength() {
+        return this._strength; 
+      }
+          
+  }
+  
+  
+  
+  class Monster extends Cards {
+      constructor(name, theme, strength) {
+        super(name, theme, strength);
+        
+       }
+   
+  }
+  
+  const monster1 = new Monster('Dracula', 'Monster', 91);
+  const monster2 = new Monster('Wolfman', 'Monster', 78);
+  const monster3 = new Monster('The Beast', 'Monster', 87);
+  
+  
+  function go(mons1, mons2) {
+    if (mons1 > mons2) {
+      return mons1;
+   } else {
+     return mons2;
+   }
+    
+  }
+  
+  go(monster2.strength, monster3.strength);
+  
+  */
